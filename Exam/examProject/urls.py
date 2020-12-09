e6fef11afbe924a0633b765631f471ce24e6ca74
menub <shahrishank@gmail.com>
2020-12-09 13:01:38 +0530

    added homepage and ui changes for student exams

--- Exam/examProject/urls.py
+++ Exam/examProject/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from . import views
 from django.urls import path,include 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -21,6 +22,7 @@ urlpatterns = [
     path('faculty/',include('faculty.urls')),
     path('student-pref/',include('studentPreferences.urls')),
     path('exams/',include('questions.urls')),
+    path('',views.index,name = "homepage")
 ]
 
 from django.conf import settings
